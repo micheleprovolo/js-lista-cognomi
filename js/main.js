ec5b28833eae5de5d0d5e01ab47f942374964c64
micheleprovolo <michele.provolo@gmail.com>
2019-10-23 15:21:21 +0200

    array ordinato

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 //DESCRIZIONE:
-
 //Chiedi all’utente il cognome,
 //inseriscilo in un array con altri cognomi
 //e stampa la lista ordinata alfabeticamente.
 //Scrivi anche la posizione della lista in cui il nuovo utente si trova
 
+
 //Chiedi all’utente il cognome
 var askSurname = prompt("Scrivi il tuo Cognome")
 
 //array cognomi disordinato
-var list = ["rossi", "verdi", "motta", "ventura"];
+var list = ["ventura", "rossi", "zucchi", "motta"];
 //console.log(list);
 
 //inserisco il cognome suggerito dall'utente nell'array disordinato
 list.push(askSurname);
+console.log(list);
+
+//stampo la lista ordinata alfabeticamente
+list.sort()
 console.log(list);
